3c81ab1270a723acfe29b715c39a8bec39af8969
neeniyuno <111344544+neeniyuno@users.noreply.github.com>
2022-11-04 01:24:50 +0800

    Update spotify.java

--- for proj/spotify.java
+++ for proj/spotify.java
@@ -3,27 +3,29 @@ import java.util.*;
 public class Spotify {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		try (Scanner input = new Scanner(System.in)) {
+			Queue_Linkedlist music = new Queue_Linkedlist();
 
-		Queue_Linkedlist music = new Queue_Linkedlist();
+			System.out.println("Welcome to the Music App");
+			while (true) {
+				
+				System.out.print("Enter your choice: ");
+				String choice = input.nextLine();
+				if (choice.equalsIgnoreCase("Insert")) {
+					System.out.print("Insert a Song: ");
+					String song = input.nextLine();
+					music.enqueue(song);
+				} else if (choice.equalsIgnoreCase("Next")) {
+					music.dequeue();
 
-		while (true) {
-			System.out.print("Enter your choice: ");
-			String choice = input.nextLine();
-			if (choice.equalsIgnoreCase("insert")) {
-				System.out.print("Insert a Song: ");
-				String song = input.nextLine();
-				music.enqueue(song);
-			} else if (choice.equalsIgnoreCase("back")) {
-				music.dequeue();
+				} else if (choice.equalsIgnoreCase("Display")) {
+					music.display();
 
-			} else if (choice.equalsIgnoreCase("display")) {
-				music.display();
+				} else if (choice.equalsIgnoreCase("Peek")) {
+					music.peek();
+				}
 
-			} else if (choice.equalsIgnoreCase("peek")) {
-				music.peek();
 			}
-
 		}
 	}
 }
