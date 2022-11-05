b39a1f2c8037ace59ed656e118a818f75499ee04
neeniyuno <111344544+neeniyuno@users.noreply.github.com>
2022-11-05 12:41:54 +0800

    Update and rename spotify.java to MusicApp.java

--- for proj/MusicApp.java
+++ for proj/MusicApp.java
@@ -1,12 +1,19 @@
 import java.util.*;
 
-public class Spotify {
+import java.util.*;
+
+public class MusicApp {
 
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
 			Queue_Linkedlist music = new Queue_Linkedlist();
 
-			System.out.println("Welcome to the Music App");
+			System.out.println("\n\nHello World, Welcome to the Music App!!");
+			System.out.println("The Queue is currently empty. \nTo play some songs, please choose Insert to input a song.");
+			System.out.println("To display the current queue, choose Display.");
+			System.out.println("To see the current song playing, choose Peek.");
+			System.out.println("If you wish to play the next song, choose Next and then Display to see the current queue, \nand Peek to see the current playing song.");
+			System.out.println("Enjoy listening!!\n\n");
 			while (true) {
 				
 				System.out.print("Enter your choice: ");
